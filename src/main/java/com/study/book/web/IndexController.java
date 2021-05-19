5d7d00cfc8c0cd24b475f4a66ea71ad53f815295
sky <skyjeong1962@gmail.com>
2021-05-19 18:39:16 +0900

    Add index

--- src/main/java/com/study/book/web/IndexController.java
+++ src/main/java/com/study/book/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.study.book.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +11,11 @@ public class IndexController {
     @GetMapping(value = "/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping(value = "/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
 }
