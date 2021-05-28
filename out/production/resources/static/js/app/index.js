0d04c50f475adadf4f9a3553f58260f6acaf006c
sky <skyjeong1962@gmail.com>
2021-05-28 16:35:09 +0900

    Update index.js

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -25,13 +25,11 @@ var main = {
         };
 
         $.ajax({
-
             type : 'POST',
             url : "/api/v1/posts",
             dataType : 'json',
             contentType : 'application/json; charset=utf-8',
             data : JSON.stringify(data)
-
         }).done(function(){
             alert('글이 등록되었습니다.');
             window.location.href = '/';
@@ -51,22 +49,16 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
-
             type: 'PUT',
             url: '/api/v1/posts/' + id,
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-
         }).done(function () {
-
             alert('글이 수정되었습니다.');
             window.location.href = '/';
-
         }).fail(function (error) {
-
             alert(JSON.stringify(error))
-
         });
 
     },
